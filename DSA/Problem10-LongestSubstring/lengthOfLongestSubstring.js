9b427eed290c799e067df1e25981f4eba64b2af9
RCThakur <rinkeshthakur1997@gmail.com>
2026-03-28 13:27:30 +0530

    Add Longest Substring problem using Sliding Window approach

---
+++ DSA/Problem10-LongestSubstring/lengthOfLongestSubstring.js
@@ -0,0 +1,23 @@
+//The Longest Substring Without Repeating Characters, Approach (Sliding Window)
+
+function lengthOfLongestSubstring(s) {
+  let set = new Set();
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+
+    set.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3
+console.log(lengthOfLongestSubstring("bbbbb")); // 1
+console.log(lengthOfLongestSubstring("pwwkew")); // 3
