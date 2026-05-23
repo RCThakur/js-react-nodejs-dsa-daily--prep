16114810f49b90f8ee10c68ab08e07bc7de6cb01
RCThakur <rinkeshthakur1997@gmail.com>
2026-05-23 19:47:46 +0530

    Add Course card to display the course details

---
+++ React/mini-course-dashboard/src/components/CourseCard.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+function CourseCard() {
+  return (
+    <div>
+      <h2>{Course.title}</h2>
+      <p>Category:{course.category}</p>
+      <p>Duration:{course.duration}</p>
+      <p>Level:{course.level}</p>
+      <p>Price:{course.price}</p>
+
+      <Link to={`/courses/${course.id}`}>
+        <button>View Details</button>
+      </Link>
+
+      <button onClick={() => saveCourse(course)}>Save Course</button>
+    </div>
+  );
+}
+
+export default CourseCard;
