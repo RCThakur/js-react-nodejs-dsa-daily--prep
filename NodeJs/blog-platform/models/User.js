85ee64db69da1ee08cfa0b0465928b6f4b545408
RCThakur <rinkeshthakur1997@gmail.com>
2026-05-31 13:26:34 +0530

    Add user schema with name password and role along with hashed password and compair password

---
+++ NodeJs/blog-platform/models/User.js
@@ -0,0 +1,46 @@
+const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    role: {
+      trpe: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+userSchema.methods.compatePassword = async function (enterPassword) {
+  return bcrypt.compare(enterPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
