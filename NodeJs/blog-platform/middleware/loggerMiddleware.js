350f01008c5f7219c71ac3b3e2a425b8e5223144
RCThakur <rinkeshthakur1997@gmail.com>
2026-05-31 13:40:52 +0530

    Add custom middleware thats  print method name URl and timestamp in every incoming request

---
+++ NodeJs/blog-platform/middleware/loggerMiddleware.js
@@ -0,0 +1,9 @@
+const { default: mongoose } = require("mongoose");
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(`${req.method} ${req.url} ${new Date().toLocaleString()}`);
+
+  next();
+};
+
+module.exports = loggerMiddleware;
