88adf37a3c96edd4f6a3ddaa2b775901ccfb4b51
RCThakur <rinkeshthakur1997@gmail.com>
2026-05-31 14:18:07 +0530

    Add auth middleware to authorization process

---
+++ NodeJs/blog-platform/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  try {
+    const token = req.header.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decode;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invaild Token",
+    });
+  }
+};
+
+module.exports = authMiddleware;
